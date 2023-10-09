278e075ddd278f9ee5ed32102d3655b687e13c0b
ellen3128 <hyunpark.ellen@gmail.com>
2023-10-09 16:41:33 -0400

    homepage updated and styled

---
+++ src/Pages/Home/HeroSection.jsx
@@ -0,0 +1,24 @@
+export default function HeroSection() {
+    return (
+        <section id="heroSection" className="hero--section">
+            <div className="hero--section--content--box">
+                <div className="hero--section--content">
+                    <p className="section--title">Hey, I'm Ellen</p>
+                    <h1 classname="hero--section--title">
+                        <span className="hero--section-title--color">Front-End</span>{" "}
+                        <br />
+                        Developer
+                    </h1>
+                    <p className="hero--section-description">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        <br /> Assumenda doloribus mollitia tempora ipsam adipisci!
+                    </p>
+                </div>
+                <button className="btn btn-primary">Get In Touch</button>
+            </div>
+            <div className="hero--section--img">
+                <img src="./img/hero_img2.png" alt="Hero Section" />
+            </div>
+        </section>
+    )
+}
