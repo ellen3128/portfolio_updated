7461cb6ae2bd4c1913f184fb948bf8ebed3294b7
ellen3128 <hyunpark.ellen@gmail.com>
2023-10-10 19:58:14 -0400

    contact icons and links added on hero and contact sections

--- src/Pages/Home/ContactMe.jsx
+++ src/Pages/Home/ContactMe.jsx
@@ -1,13 +1,43 @@
+import linkedinIcon from "../../icon/linkedin.png";
+import githubIcon from "../../icon/github.png";
+import gmailIcon from "../../icon/gmail.png";
+
 export default function ContactMe() {
   return (
     <section id="Contact" className="contact--section">
-    <div>
-      <p className="sub--title2">Get In Touch</p>
-      <h2>Contact Me</h2>
-      <p className="text-lg">
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. In, odit.
-      </p>
-    </div>
+      <div>
+        <p className="sub--title2">Get In Touch</p>
+        <h2>Contact Me</h2>
+        <p className="text-lg">
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. In, odit.
+        </p>
+      </div>
+      <div>
+        <a href="https://www.linkedin.com/in/hyunellenpark1/">
+          <img
+            style={{ width: "60px", height: "60px", margin: "10px" }}
+            src={linkedinIcon}
+            alt="LinkedIn Icon"
+          />
+        </a>
+        <a href="https://github.com/ellen3128">
+          <img
+            style={{ width: "55px", height: "55px", margin: "10px" }}
+            src={githubIcon}
+            alt="Github Icon"
+          />
+        </a>
+        <a
+              href="https://mail.google.com/mail/?view=cm&source=mailto&to=hyunpark.ellen@gmail.com"
+              target="_blank"
+            >
+              <img
+                style={{ width: "55px", height: "55px", margin: "10px" }}
+                src={gmailIcon}
+                alt="Gmail Icon"
+              />
+            </a>
+      </div>
     </section>
-)
+  );
 }
--- src/Pages/Home/HeroSection.jsx
+++ src/Pages/Home/HeroSection.jsx
@@ -1,25 +1,53 @@
-export default function HeroSection() {
-    return (
-        <section id="heroSection" className="hero--section">
-            <div className="hero--section--content--box">
-                <div className="hero--section--content">
-                    <p className="section--title">Hey, I'm Ellen</p>
-                    <h1 classname="hero--section--title">
-                        <span className="hero--section-title--color">Front-End</span>{" "}
-                        <br />
-                        Developer
-                    </h1>
-                    <p className="hero--section-description">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                        <br /> Assumenda doloribus mollitia tempora ipsam adipisci!
-                    </p>
-                </div>
-                <button className="btn btn-primary">Get In Touch</button>
-            </div>
-            <div className="hero--section--img">
-                <img src="./img/hero_img4.png" alt="Hero Section" />
+import linkedinIcon from "../../icon/linkedin.png";
+import githubIcon from "../../icon/github.png";
+import gmailIcon from "../../icon/gmail.png";
 
-            </div>
-        </section>
-    )
+export default function HeroSection() {
+  return (
+    <section id="heroSection" className="hero--section">
+      <div className="hero--section--content--box">
+        <div className="hero--section--content">
+          <p className="section--title">Hey, I'm Ellen</p>
+          <h1 classname="hero--section--title">
+            <span className="hero--section-title--color">Front-End</span> <br />
+            Developer
+          </h1>
+          <p className="hero--section-description">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            <br /> Assumenda doloribus mollitia tempora ipsam adipisci!
+          </p>
+          <div className="contact--link--icons">
+            <a href="https://www.linkedin.com/in/hyunellenpark1/">
+              <img
+                style={{ width: "60px", height: "60px", margin: "10px" }}
+                src={linkedinIcon}
+                alt="LinkedIn Icon"
+              />
+            </a>
+            <a href="https://github.com/ellen3128">
+              <img
+                style={{ width: "55px", height: "55px", margin: "10px" }}
+                src={githubIcon}
+                alt="Github Icon"
+              />
+            </a>
+            <a
+              href="https://mail.google.com/mail/?view=cm&source=mailto&to=hyunpark.ellen@gmail.com"
+              target="_blank"
+            >
+              <img
+                style={{ width: "55px", height: "55px", margin: "10px" }}
+                src={gmailIcon}
+                alt="Gmail Icon"
+              />
+            </a>
+          </div>
+        </div>
+        <button className="btn btn-primary">Get In Touch</button>
+      </div>
+      <div className="hero--section--img">
+        <img src="./img/hero_img4.png" alt="Hero Section" />
+      </div>
+    </section>
+  );
 }
