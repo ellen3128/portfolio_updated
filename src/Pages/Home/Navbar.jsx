ebd6675818dcb264461ed8222a83bc33c6710b96
ellen3128 <hyunpark.ellen@gmail.com>
2023-10-10 20:12:17 -0400

    Navbar modified to include skills section

--- src/Pages/Home/Navbar.jsx
+++ src/Pages/Home/Navbar.jsx
@@ -68,6 +68,20 @@ export default function Navbar() {
               smooth={true}
               offset={-70}
               duration={500}
+              to="mySkills"
+              className="navbar--content"
+            >
+              My Skills
+            </Link>
+          </li>
+          <li>
+            <Link
+              onclick={closeMenu}
+              activeClass="navbar--active-content"
+              spy={true}
+              smooth={true}
+              offset={-70}
+              duration={500}
               to="AboutMe"
               className="navbar--content"
             >
